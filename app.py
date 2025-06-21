bcbeb745e9af9f9e1ccc07cc0ac8085edcad6ace
nhan3108 <nguyennhanytbbbb@gmail.com>
2025-06-21 20:37:41 +0700

    Initial commit

--- app.py
+++ app.py
@@ -122,5 +122,6 @@ def du_doan():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 1234))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+
